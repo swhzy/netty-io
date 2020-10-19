bd16ee3f8a04d73674aaaf9c339bf1ea92924f8d
songweihui <songweihui@chengtay.com>
2020-10-19 16:00:08 +0800

    bio server and client

---
+++ src/main/java/com/swh/io/bio/BioClientSocket.java
@@ -0,0 +1,44 @@
+package com.swh.io.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * Copyright (C), 2018-2020
+ * FileName: BioClientSocket
+ * Author:   songweihui
+ * Date:     2020/10/16 17:10
+ * Description: bio 客户端socket
+ */
+public class BioClientSocket {
+
+    private static int defaultPort = 8081;
+
+
+    public static void main(String[] args) {
+
+        Socket socket = null;
+        BufferedReader  in = null;
+        PrintWriter pw = null;
+        try {
+            socket = new Socket("127.0.0.1", defaultPort);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pw = new PrintWriter(socket.getOutputStream());
+
+
+            pw.println("xxxxxxxxxx\n");
+            pw.flush();
+            String readLine = in.readLine();
+            System.out.println("this is client ,receive server data:"+readLine);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/swh/io/bio/BioServerHandler.java
+++ src/main/java/com/swh/io/bio/BioServerHandler.java
@@ -1,5 +1,8 @@
 package com.swh.io.bio;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -19,6 +22,57 @@ public class BioServerHandler implements Runnable{
 
     @Override
     public void run() {
+        if(socket==null){
+            System.out.println("获取字节流异常");
+        }
+        BufferedReader in = null;
+        PrintWriter pw = null;
+        ByteInputStream bs = null;
+        try {
+            InputStream inputStream = socket.getInputStream();
+//            byte[] bytes = new byte[1024];
+//            while (inputStream.read(bytes)!=-1){
+//                String s = new String(bytes);
+//                System.out.println(s);
+//            }
+
+            in = new BufferedReader(new InputStreamReader(inputStream));
+
+            pw = new PrintWriter(socket.getOutputStream());
+            while (true){
+                String readLine = in.readLine();
+                if("".equals(readLine)){
+                    break;
+                }
+                System.out.println("bio server handler receive data:"+readLine);
+            }
+
+            pw.println("接受到了,over\n");
+            pw.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(in != null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(pw!=null){
+                pw.close();
+                pw = null;
+            }
+            if(socket!=null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
     }
 }
